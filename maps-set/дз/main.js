572050d681a6f0243defbfcb18f19fa364846434
igorbel <igor.belokrys.96@bk.ru>
2024-12-28 16:59:22 +0300

    переделал дз с использованием map

--- maps-set/дз/main.js
+++ maps-set/дз/main.js
@@ -6,6 +6,19 @@ let arr = [
   { id: 1, name: 'Ann' },
 ];
 
-let set = new Set(arr.map((item) => JSON.stringify(item)));
-let setObj = new Set([...set].map((item) => JSON.parse(item)));
-console.log(setObj);
+// let set = new Set(arr.map((item) => JSON.stringify(item)));
+// let setObj = new Set([...set].map((item) => JSON.parse(item)));
+// console.log(setObj);
+
+let set = new Set();
+
+let newArr = arr.map((elem) => {
+  if (![...set].find((item) => item.id === elem.id)) {
+    set.add(elem);
+    return elem;
+  } else {
+    return false;
+  }
+});
+
+console.log(set);
